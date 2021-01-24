8610510020c68cb87f7673c7f6b74961aa0b199c
MD Danish Ansari <ansarid567@gmail.com>
2021-01-24 21:08:01 +0530

    Added ViewBinding in PictureListFragment.kt

--- app/src/main/java/com/md/nasaapod/picture_list/ui/PictureListFragment.kt
+++ app/src/main/java/com/md/nasaapod/picture_list/ui/PictureListFragment.kt
@@ -4,9 +4,13 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import com.md.nasaapod.R
+import com.md.nasaapod.databinding.FragmentPictureListBinding
+import com.md.nasaapod.utils.viewBinding
 
 
 class PictureListFragment : Fragment(R.layout.fragment_picture_list) {
+    private val binding by viewBinding(FragmentPictureListBinding::bind)
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
