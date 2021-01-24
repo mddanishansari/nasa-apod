5abade50658ef0ab65de49fb94b2b9d0e7c51da2
MD Danish Ansari <ansarid567@gmail.com>
2021-01-24 20:16:59 +0530

    Setup hilt

---
+++ app/src/main/java/com/md/nasaapod/App.kt
@@ -0,0 +1,7 @@
+package com.md.nasaapod
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App : Application()
--- app/src/main/java/com/md/nasaapod/MainActivity.kt
+++ app/src/main/java/com/md/nasaapod/MainActivity.kt
@@ -4,8 +4,9 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.md.nasaapod.databinding.ActivityMainBinding
 import com.md.nasaapod.utils.viewBinding
+import dagger.hilt.android.AndroidEntryPoint
 
-
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private val binding by viewBinding(ActivityMainBinding::inflate)
 
