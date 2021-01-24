ec461b2f744db683fbee3bb9d46bc617a4eb97ef
MD Danish Ansari <ansarid567@gmail.com>
2021-01-24 20:50:13 +0530

    Added Picture list screen

---
+++ app/src/main/java/com/md/nasaapod/picture_list/ui/PictureListFragment.kt
@@ -0,0 +1,14 @@
+package com.md.nasaapod.picture_list.ui
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.md.nasaapod.R
+
+
+class PictureListFragment : Fragment(R.layout.fragment_picture_list) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
